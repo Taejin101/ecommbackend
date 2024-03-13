f897a9ebd56789878af935c1be214c864af46637
ishanjoshi <ishanjoshiian@gmail.com>
2024-03-13 13:15:19 +0530

    login Api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 
 const bcrypt = require('bcryptjs')
 const user_model = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const secret = require('../configs/auth.config')
 
 exports.signup = async (req, res) => {
     /*
@@ -37,4 +39,39 @@ exports.signup = async (req, res) => {
             message : "Some error occurred while registering the user"
         })
     }
+}
+
+
+exports.signin = async (req, res) => {
+
+    // check if user id is present in the system
+    const user = await user_model.findOne({userid : req.body.userid})
+
+    if(user == null) {
+        return res.status(400).send({
+            "message" : "userid is not present"
+        })
+    }
+
+    // password is correct
+    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password)
+
+    if(!isPasswordValid) {
+        return res.status(401).send({
+            "message" : "Incorrect password"
+        })
+    }
+
+    // using jwt we will create an access token with a given TTL and return
+    const token = jwt.sign({id : user.userid}, secret.secret, {
+        expiresIn : 120
+    })
+
+    return res.status(200).send({
+        name : user.name,
+        userid : user.userid,
+        email : user.email,
+        usertype : user.usertype,
+        accesstoken : token
+    })
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,4 +4,11 @@ const authMW = require('../middlewares/auth.mw')
 
 module.exports = (app) => {
     app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
+
+    /**
+     * route for
+     * POST localhost:8888/ecomm/api/v1/auth/signin
+     */
+
+    app.post("/ecomm/api/v1/auth/signin", authController.signin)
 }
